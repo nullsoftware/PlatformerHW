08e80ee77128d89edcb32316690f09969199fbcd
nullsoftware <tumyrthebot@gmail.com>
2021-09-15 19:30:41 +0300

    Removed unused namespaces.

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(EntityStats))]
 [RequireComponent(typeof(Collider2D))]
